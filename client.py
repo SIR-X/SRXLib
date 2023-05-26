531f7263a40533945fe7c18763c2370b4d1c06a4
S!R X <111647198+SIR-X@users.noreply.github.com>
2023-05-26 14:00:03 +0330

    Update Client
    Delete and set new webhook

--- client.py
+++ client.py
@@ -5,11 +5,14 @@ from fastapi import FastAPI, Request
 from orjson  import loads
 
 class Bot(FastAPI, Methods):
-    def __init__(self, bot_token: str):
+    def __init__(self, bot_token: str, webhook_url: str):
         Methods.__init__(self)
         FastAPI.__init__(self)
 
         self.api = f"https://api.telegram.org/bot{bot_token}/"
+
+        self.delete_webhook()
+        self.set_webhook(webhook_url)
 
     def on_update(self, webhook: str = "/webhook"):
         def decorator(func):
@@ -17,6 +20,15 @@ class Bot(FastAPI, Methods):
             async def wrapper(request: Request):
                 update = loads(await request.body())
                 return await func(self, update)
+            return wrapper
+        return decorator
+
+    def on_callback_query(self):
+        def decorator(func):
+            @self.on_update()
+            async def wrapper(self, update: dict):
+                if "callback_query" in update:
+                    return await func(self, update["callback_query"])
             return wrapper
         return decorator
 
